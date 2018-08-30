862b552b4b118b357cca7d0f4be9fed16299bd5e
Jayon Kim <jpkim921@gmail.com>
2018-08-30 02:01:21 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  validates :content, length: { minimum: 1 }
+  validates :content, length: { minimum: 100 }
   validates :category, presence: true
-  
+
 end
