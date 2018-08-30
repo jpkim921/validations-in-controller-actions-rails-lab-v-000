016b8264b98f060231f71c4004483837fb590785
Jayon Kim <jpkim921@gmail.com>
2018-08-30 01:17:40 +0000

    automatically backed up by learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -13,7 +13,7 @@ class AuthorsController < ApplicationController
     if @author.valid?
       @author.save
 
-    redirect_to author_path(@author)
+      redirect_to author_path(@author)
     else
       render :new
   end
