fe91870560cc25c6d5f8a31aa5752c0700d01270
Jayon Kim <jpkim921@gmail.com>
2018-08-30 02:09:05 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,8 +10,7 @@ class PostsController < ApplicationController
   def update
     @post.update(post_params)
 
-    if @post.valid?
-      @post.save
+    if @post.save
       redirect_to post_path(@post)
     else
       render :edit
